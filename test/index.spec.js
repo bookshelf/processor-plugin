3150744ab41db94455e5e7ad61259238550479d0
Ricardo Graça <ricardo@devius.net>
2019-08-04 19:46:18 +0100

    Add processor plugin code from Bookshelf

---
+++ test/index.spec.js
@@ -0,0 +1,101 @@
+const { equal, throws } = require('assert')
+const knex = require('knex')
+const db = knex({ client: 'sqlite3', connection: ':memory:', useNullAsDefault: true })
+const bookshelf = require('bookshelf')(db)
+const processor = require('..')
+
+describe('Processor Plugin', function() {
+  var User
+
+  function lowerCaseProcessor(string) {
+    return string.toLowerCase()
+  }
+
+  function trimProcessor(string) {
+    return string.trim()
+  }
+
+  before(function() {
+    bookshelf.plugin(processor)
+    User = bookshelf.Model.extend({ tableName: 'users' })
+  })
+
+  describe('Model#processors', function() {
+    it('is false by default', function() {
+      equal(new User().processors, false)
+    })
+  })
+
+  describe('Model#set()', function() {
+    it("throws an error if the specified processor doesn't exist", function() {
+      var OtherUser = User.extend({
+        processors: {
+          username: 'nothingHere'
+        }
+      })
+
+      function setUsername() {
+        new OtherUser().set('username', 'test')
+      }
+
+      throws(setUsername)
+    })
+
+    it('processes the set attribute with the correct processor', function() {
+      var OtherUser = User.extend({
+        processors: {
+          username: lowerCaseProcessor
+        }
+      })
+      var otherUser = new OtherUser().set('username', 'TesT')
+
+      equal(otherUser.get('username'), 'test')
+    })
+
+    it('can accept an object with attributes to process', function() {
+      var OtherUser = User.extend({
+        processors: {
+          username: lowerCaseProcessor
+        }
+      })
+      var otherUser = new OtherUser().set({ username: 'TesT' })
+
+      equal(otherUser.get('username'), 'test')
+    })
+
+    it('can process an attribute with multiple processors', function() {
+      var OtherUser = User.extend({
+        processors: {
+          username: [lowerCaseProcessor, trimProcessor]
+        }
+      })
+      var otherUser = new OtherUser().set('username', 'TesT   ')
+
+      equal(otherUser.get('username'), 'test')
+    })
+
+    it("doesn't do any processing if no processors are specified", function() {
+      var OtherUser = User.extend({
+        processors: {
+          bogus: [lowerCaseProcessor]
+        }
+      })
+      var otherUser = new OtherUser().set('username', 'TesT')
+
+      equal(otherUser.get('username'), 'TesT')
+    })
+
+    it('can use an anonymous processor function', function() {
+      var OtherUser = User.extend({
+        processors: {
+          username: function(string) {
+            return string + '_custom'
+          }
+        }
+      })
+      var otherUser = new OtherUser().set('username', 'TesT')
+
+      equal(otherUser.get('username'), 'TesT_custom')
+    })
+  })
+})
