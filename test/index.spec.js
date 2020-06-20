5efc04c09ba65be2ca0a3a676f2389cb48bdaeec
Ricardo Graça <ricardo@devius.net>
2020-06-20 16:03:49 +0100

    Fix linter errors

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,7 +5,7 @@ const db = knex({ client: 'sqlite3', connection: ':memory:', useNullAsDefault: t
 const bookshelf = require('bookshelf')(db)
 const processor = require('..')
 
-describe('Processor Plugin', function() {
+describe('Processor Plugin', function () {
   let User
 
   function lowerCaseProcessor(string) {
@@ -21,14 +21,14 @@ describe('Processor Plugin', function() {
     User = bookshelf.Model.extend({ tableName: 'users' })
   })
 
-  describe('Model#processors', function() {
-    it('is false by default', function() {
+  describe('Model#processors', function () {
+    it('is false by default', function () {
       equal(new User().processors, false)
     })
   })
 
-  describe('Model#set()', function() {
-    it("throws an error if the specified processor doesn't exist", function() {
+  describe('Model#set()', function () {
+    it("throws an error if the specified processor doesn't exist", function () {
       const OtherUser = User.extend({
         processors: { username: 'nothingHere' }
       })
@@ -36,7 +36,7 @@ describe('Processor Plugin', function() {
       throws(() => new OtherUser().set('username', 'test'))
     })
 
-    it('processes the set attribute with the correct processor', function() {
+    it('processes the set attribute with the correct processor', function () {
       const OtherUser = User.extend({
         processors: { username: lowerCaseProcessor }
       })
@@ -45,7 +45,7 @@ describe('Processor Plugin', function() {
       equal(otherUser.get('username'), 'test')
     })
 
-    it('can accept an object with attributes to process', function() {
+    it('can accept an object with attributes to process', function () {
       const OtherUser = User.extend({
         processors: { username: lowerCaseProcessor }
       })
@@ -54,7 +54,7 @@ describe('Processor Plugin', function() {
       equal(otherUser.get('username'), 'test')
     })
 
-    it('can process an attribute with multiple processors', function() {
+    it('can process an attribute with multiple processors', function () {
       const OtherUser = User.extend({
         processors: { username: [lowerCaseProcessor, trimProcessor] }
       })
@@ -63,7 +63,7 @@ describe('Processor Plugin', function() {
       equal(otherUser.get('username'), 'test')
     })
 
-    it("doesn't do any processing if no processors are specified", function() {
+    it("doesn't do any processing if no processors are specified", function () {
       const OtherUser = User.extend({
         processors: { bogus: [lowerCaseProcessor] }
       })
@@ -72,10 +72,10 @@ describe('Processor Plugin', function() {
       equal(otherUser.get('username'), 'TesT')
     })
 
-    it('can use an anonymous processor function', function() {
+    it('can use an anonymous processor function', function () {
       const OtherUser = User.extend({
         processors: {
-          username: string => string + '_custom'
+          username: (string) => string + '_custom'
         }
       })
       const otherUser = new OtherUser().set('username', 'TesT')
@@ -83,7 +83,7 @@ describe('Processor Plugin', function() {
       equal(otherUser.get('username'), 'TesT_custom')
     })
 
-    it('does nothing if not passing a key name', function() {
+    it('does nothing if not passing a key name', function () {
       const spiedProcess = sinon.spy(bookshelf.Model.prototype, 'processAttribute')
       const OtherUser = User.extend({
         processors: { username: lowerCaseProcessor }
@@ -94,7 +94,7 @@ describe('Processor Plugin', function() {
       equal(spiedProcess.called, false)
     })
 
-    it('passes the model context as "this" to each processor', function() {
+    it('passes the model context as "this" to each processor', function () {
       const OtherUser = User.extend({
         processors: {
           username(value) {
